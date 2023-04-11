1932d484db43004e9ed1d91440600d70a3d8861c
Đoàn Quang Huy <quanghuybest@gmail.com>
2023-04-11 20:21:51 +0700

    hoàn thành album

--- react-project/src/components/frontend/Album.js
+++ react-project/src/components/frontend/Album.js
@@ -1,42 +1,57 @@
-import React, { useEffect, useState } from 'react';
-import { Link } from 'react-router-dom';
-import $ from 'jquery';
-import 'magnific-popup';
-import './css/album.css';
-import axios from 'axios';
-import config from '../../config';
+import React, { useEffect, useState } from "react";
+import { Link, useHistory } from "react-router-dom";
+import $ from "jquery";
+import "magnific-popup";
+import "./css/album.css";
+import axios from "axios";
+import config from "../../config";
+import swal from "sweetalert";
 
 const Gallery = () => {
+    const [categorylist, setCategorylist] = useState([]);
     const [petList, setPetList] = useState([]);
-    const [activeCategory, setActiveCategory] = useState('all');
-
+    const [loading, setLoading] = useState(true);
+    const [activeCategory, setActiveCategory] = useState("all");
+    const history = useHistory();
     const handleCategoryChange = (category) => {
         setActiveCategory(category);
 
-        if (category === 'all') {
-            $('.image').show(400);
+        if (category === "all") {
+            $(".image").show(400);
         } else {
-            $('.image')
-                .not(`.${category}`)
-                .hide(200);
+            $(".image").not(`.${category}`).hide(200);
             $(`.image.${category}`).show(400);
         }
     };
     useEffect(() => {
         let isMounted = true;
 
-        axios.get(`/api/getAlbumPet`).then(res => {
+        axios.get(`/api/getAlbumPet`).then((res) => {
             if (isMounted) {
                 if (res.data.status === 200) {
                     setPetList(res.data.pets);
+                    setLoading(false);
                 }
             }
         });
 
         return () => {
-            isMounted = false
+            isMounted = false;
         };
+    }, []);
+    useEffect(() => {
+        let isMounted = true;
 
+        axios.get(`/api/get-all-category`).then((res) => {
+            if (isMounted) {
+                if (res.data.status === 200) {
+                    setCategorylist(res.data.category);
+                }
+            }
+        });
+        return () => {
+            isMounted = false;
+        };
     }, []);
     const renderImages = () =>
         petList.map((pet, index) => (
@@ -47,62 +62,83 @@ const Gallery = () => {
             >
                 <span className="username">{pet.user.name}</span>
                 <img src={`${config.BASE_URL}/${pet.image_pet}`} alt="" />
-                <span className='emotion'>{pet.emotion}</span>
+                <span className="emotion">{pet.emotion}</span>
             </a>
         ));
 
-    return (
-        <div className="gallery">
-            <ul className="controls">
-                <li
-                    className={`buttons ${activeCategory === 'all' ? 'active' : ''}`}
-                    onClick={() => handleCategoryChange('all')}
-                    data-filter="all"
-                >
-                    Tất cả
-                </li>
-                <li
-                    className={`buttons ${activeCategory === 'cho' ? 'active' : ''
-                        }`}
-                    onClick={() => handleCategoryChange('cho')}
-                    data-filter="cho"
-                >
-                    Chó
-                </li>
-                <li
-                    className={`buttons ${activeCategory === 'meo' ? 'active' : ''
-                        }`}
-                    onClick={() => handleCategoryChange('meo')}
-                    data-filter="meo"
-                >
-                    Mèo
-                </li>
-                <li
-                    className={`buttons ${activeCategory === 'chim' ? 'active' : ''
-                        }`}
-                    onClick={() => handleCategoryChange('chim')}
-                    data-filter="chim"
-                >
-                    Chim
-                </li>
-                <li>
-                    <Link to={"/addPet"} className='btn btn-primary'>
-                        Thêm thú cưng
-                    </Link>
-                </li>
-            </ul>
+    const submitAddPet = () => {
+        if (!localStorage.getItem("auth_token")) {
+            // chua dang nhap
+            swal("Lỗi rồi cưng ơi", "Bạn phải đăng nhập", "warning");
+            history.push("/login");
+        } else {
+            history.push("/addPet");
+        }
+    };
+    // hiển thị theo loại
+    const renderCategories = () =>
+        categorylist.map((item) => (
+            <li
+                key={item.id}
+                className={`buttons ${activeCategory === item.slug ? "active" : ""}`}
+                onClick={() => handleCategoryChange(item.slug)}
+                data-filter={item.slug}
+            >
+                {item.name}
+            </li>
+        ));
 
-            <div className="image-container">{renderImages()}</div>
-        </div>
-    )
-}
+    if (loading) {
+        return <h4>Xin vui lòng chờ...</h4>;
+    } else {
+        if (petList.length > 0) {
+            return (
+                <div className="gallery">
+                    <ul className="controls">
+                        <li
+                            className={`buttons ${activeCategory === 'all' ? 'active' : ''}`}
+                            onClick={() => handleCategoryChange('all')}
+                            data-filter="all"
+                        >
+                            Tất cả
+                        </li>
+                        {renderCategories()}
+                        <li>
+                            <button className="btn btn-primary" onClick={submitAddPet}>
+                                Thêm thú cưng
+                            </button>
+                        </li>
+                    </ul>
+                    <div className="image-container">{renderImages()}</div>
+                </div>
+            );
+        } else {
+            return (
+                <>
+                    <div className="gallery">
+                        <ul className="controls">
+                            <li>
+                                <button className="btn btn-primary" onClick={submitAddPet}>
+                                    Thêm thú cưng
+                                </button>
+                            </li>
+                        </ul>
+                        <h4 className="text-danger text-center">
+                            Hiện chưa có gì! rất xin lỗi về vấn đề này.
+                        </h4>
+                    </div>
+                </>
+            );
+        }
+    }
+};
 
 function Album() {
     return (
-        <div className='main'>
+        <div className="main">
             <Gallery />
         </div>
-    )
+    );
 }
 
 export default Album;
