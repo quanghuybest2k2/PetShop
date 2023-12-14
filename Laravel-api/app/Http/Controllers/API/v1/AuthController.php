bf2d04f2667ba633847f07729691e2b824a96b35
Doan Quang Huy <quanghuybest@gmail.com>
2023-12-14 20:14:09 +0700

    add /api/*

--- Laravel-api/app/Http/Controllers/API/v1/AuthController.php
+++ Laravel-api/app/Http/Controllers/API/v1/AuthController.php
@@ -37,44 +37,48 @@ class AuthController extends Controller
      */
     public function register(Request $request): JsonResponse
     {
-        $validator = Validator::make(
-            $request->all(),
-            [
-                'name' => 'required|max:191',
-                'email' => 'required|email|max:191|unique:users,email',
-                'password' => 'required|min:8',
-            ],
-            [
-                'required' => 'Bạn phải điền :attribute',
-                'max' => ':attribute không được vượt quá :max ký tự',
-                'email' => ':attribute không hợp lệ',
-                'unique' => ':attribute đã được sử dụng',
-                'min' => ':attribute phải có ít nhất :min ký tự',
-            ],
-            [
-                'name' => 'Họ và tên',
-                'email' => 'Email',
-                'password' => 'Mật khẩu',
-            ]
-        );
-        if ($validator->fails()) {
-            return response()->json([
-                'status' => Response::HTTP_BAD_REQUEST,
-                'errors' => $validator->errors(),
-            ], Response::HTTP_BAD_REQUEST);
-        } else {
-            $user = User::create([
-                'name' => $request->name,
-                'email' => $request->email,
-                'password' => Hash::make($request->password),
-            ]);
-            $token = $user->createToken($user->email . '_Token')->plainTextToken;
-            return response()->json([
-                'status' => Response::HTTP_OK,
-                'username' => $user->name,
-                'token' => $token,
-                'message' => 'Đăng ký thành công.',
-            ]);
+        try {
+            $validator = Validator::make(
+                $request->all(),
+                [
+                    'name' => 'required|max:191',
+                    'email' => 'required|email|max:191|unique:users,email',
+                    'password' => 'required|min:8',
+                ],
+                [
+                    'required' => 'Bạn phải điền :attribute',
+                    'max' => ':attribute không được vượt quá :max ký tự',
+                    'email' => ':attribute không hợp lệ',
+                    'unique' => ':attribute đã được sử dụng',
+                    'min' => ':attribute phải có ít nhất :min ký tự',
+                ],
+                [
+                    'name' => 'Họ và tên',
+                    'email' => 'Email',
+                    'password' => 'Mật khẩu',
+                ]
+            );
+            if ($validator->fails()) {
+                return response()->json([
+                    'status' => Response::HTTP_BAD_REQUEST,
+                    'errors' => $validator->errors(),
+                ], Response::HTTP_BAD_REQUEST);
+            } else {
+                $user = User::create([
+                    'name' => $request->name,
+                    'email' => $request->email,
+                    'password' => Hash::make($request->password),
+                ]);
+                $token = $user->createToken($user->email . '_Token')->plainTextToken;
+                return response()->json([
+                    'status' => Response::HTTP_OK,
+                    'username' => $user->name,
+                    'token' => $token,
+                    'message' => 'Đăng ký thành công.',
+                ]);
+            }
+        } catch (\Exception $e) {
+            return $this->responseError(null, $e->getMessage(), Response::HTTP_INTERNAL_SERVER_ERROR);
         }
     }
     /**
@@ -96,49 +100,53 @@ class AuthController extends Controller
      */
     public function login(Request $request): JsonResponse
     {
-        $validator = Validator::make(
-            $request->all(),
-            [
-                'email' => 'required|max:255',
-                'password' => 'required|min:8',
-            ],
-            [
-                'required'  => 'Bạn phải điền :attribute',
-                'max' => ':attribute không được vượt quá :max ký tự',
-                'min' => ':attribute phải có ít nhất :min ký tự',
-            ],
-            [
-                'email' => 'Email',
-                'password' => 'Mật khẩu'
-            ]
-        );
-        if ($validator->fails()) {
-            return response()->json([
-                'validator_errors' => $validator->messages(),
-            ]);
-        } else {
-            $user = User::where('email', $request->email)->first();
-            if (!$user || !Hash::check($request->password, $user->password)) {
+        try {
+            $validator = Validator::make(
+                $request->all(),
+                [
+                    'email' => 'required|max:255',
+                    'password' => 'required|min:8',
+                ],
+                [
+                    'required'  => 'Bạn phải điền :attribute',
+                    'max' => ':attribute không được vượt quá :max ký tự',
+                    'min' => ':attribute phải có ít nhất :min ký tự',
+                ],
+                [
+                    'email' => 'Email',
+                    'password' => 'Mật khẩu'
+                ]
+            );
+            if ($validator->fails()) {
                 return response()->json([
-                    'status' => Response::HTTP_UNAUTHORIZED,
-                    'message' => 'Thông tin không hợp lệ!',
-                ], Response::HTTP_UNAUTHORIZED);
-            } else {
-                if ($user->role_as == 1) { // admin
-                    $role = 'admin';
-                    $token = $user->createToken($user->email . '_AdminToken', ['server:admin'])->plainTextToken;
-                } else {
-                    $role = '';
-                    $token = $user->createToken($user->email . '_Token', [''])->plainTextToken;
-                }
-                return response()->json([
-                    'status' => Response::HTTP_OK,
-                    'username' => $user->name,
-                    'token' => $token,
-                    'message' => 'Đăng nhập thành công.',
-                    'role' => $role,
+                    'validator_errors' => $validator->messages(),
                 ]);
+            } else {
+                $user = User::where('email', $request->email)->first();
+                if (!$user || !Hash::check($request->password, $user->password)) {
+                    return response()->json([
+                        'status' => Response::HTTP_UNAUTHORIZED,
+                        'message' => 'Thông tin không hợp lệ!',
+                    ], Response::HTTP_UNAUTHORIZED);
+                } else {
+                    if ($user->role_as == 1) { // admin
+                        $role = 'admin';
+                        $token = $user->createToken($user->email . '_AdminToken', ['server:admin'])->plainTextToken;
+                    } else {
+                        $role = '';
+                        $token = $user->createToken($user->email . '_Token', [''])->plainTextToken;
+                    }
+                    return response()->json([
+                        'status' => Response::HTTP_OK,
+                        'username' => $user->name,
+                        'token' => $token,
+                        'message' => 'Đăng nhập thành công.',
+                        'role' => $role,
+                    ]);
+                }
             }
+        } catch (\Exception $e) {
+            return $this->responseError(null, $e->getMessage(), Response::HTTP_INTERNAL_SERVER_ERROR);
         }
     }
     /**
